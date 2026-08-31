94da22bf52738d55c7ce30de175066cf451f858e
EmanuelPlaysDev <EmanuelPlaysDev@hotmail.com>
2026-08-31 15:10:01 +0200

    Update SpigBan.java

--- src/main/java/dev/emanuelplays/spigban/SpigBan.java
+++ src/main/java/dev/emanuelplays/spigban/SpigBan.java
@@ -12,7 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 /**
- * @version 1.0.0
+ * @version 2.0.0
  */
 public class SpigBan extends JavaPlugin {
 
